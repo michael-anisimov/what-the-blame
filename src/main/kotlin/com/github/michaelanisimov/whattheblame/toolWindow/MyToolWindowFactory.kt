b771247bd8c36b066d50652cddd4bff710ae22ee
GitHub Action <action@github.com>
2026-04-20 18:36:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/michaelanisimov/whattheblame/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/michaelanisimov/whattheblame/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.michaelanisimov.whattheblame.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.michaelanisimov.whattheblame.MyBundle
+import com.github.michaelanisimov.whattheblame.services.MyProjectService
 import javax.swing.JButton
 
 
